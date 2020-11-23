6242e3c7056b3bdd866ea0e30d9f33f4a0ea3a72
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-11-23 22:04:17 +0530

    Update flask_predict_csv.py

--- flask_predict_csv.py
+++ flask_predict_csv.py
@@ -9,7 +9,7 @@ with open('/content/sample_data/wine_rf.pkl', 'rb') as model_file:
 app=Flask(__name__)
 @app.route('/predict_file', methods=['POST'])
 def predict_wine_file():
-    input_data = pd.read_csv(request.files.get["input_file"])
+    input_data = pd.read_csv(request.files.get["input_file"], header=None)
     prediction = model.predict(input_data)
     return str(list(prediction))
 if __name__=='__main__':
